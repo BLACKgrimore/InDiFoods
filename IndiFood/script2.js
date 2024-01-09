8e470bdc7ff2104156e7cb9240f092eaf071ca49
Roshan Dubey <roshan2021cs150@abesit.edu.in>
2024-01-09 14:04:59 +0530

    R1.1
    Merging the signup page

--- IndiFood/script2.js
+++ IndiFood/script2.js
@@ -35,3 +35,10 @@
     }
 
     setInterval(nextSlide, 3000); // Auto slide every 3 seconds
+
+    /* Hamburger Icon */
+    function toggleNavbar() {
+        var menu = document.getElementById("navbarMenu");
+        menu.classList.toggle("change");
+    }
+    
